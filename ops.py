5db7aaf9819672a611aa3cd05815902f7f618f03
Jimmy Johnson <jay4842@gmail.com>
2018-07-23 14:53:31 -0500

    added some comments

--- ops.py
+++ ops.py
@@ -6,26 +6,30 @@ from tensorflow.python.framework import ops
 
 from utils import *
 
+# our batch norm boy
 def batch_norm(x, name="batch_norm"):
     return tf.contrib.layers.batch_norm(x, decay=0.9, updates_collections=None, epsilon=1e-5, scale=True, scope=name)
 
+# normalizes a layer maybe
 def instance_norm(input, name="instance_norm"):
     with tf.variable_scope(name):
         depth = input.get_shape()[3]
         scale = tf.get_variable("scale", [depth], initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
         offset = tf.get_variable("offset", [depth], initializer=tf.constant_initializer(0.0))
-        mean, variance = tf.nn.moments(input, axes=[1,2], keep_dims=True)
+        mean, variance = tf.nn.moments(input, axes=[1,2], keep_dims=True) # uses mean and variance for normalizing
         epsilon = 1e-5
-        inv = tf.rsqrt(variance + epsilon)
-        normalized = (input-mean)*inv
-        return scale*normalized + offset
+        inv = tf.rsqrt(variance + epsilon) #tf.rsqrt y = 1 / srt(x)
+        normalized = (input-mean)*inv # subtract the mean from the input and multiply by the inveriance
+        return scale*normalized + offset # add offset and scale the input as well
 
+# using slim with this
 def conv2d(input_, output_dim, ks=4, s=2, stddev=0.02, padding='SAME', name="conv2d"):
     with tf.variable_scope(name):
         return slim.conv2d(input_, output_dim, ks, s, padding=padding, activation_fn=None,
                             weights_initializer=tf.truncated_normal_initializer(stddev=stddev),
                             biases_initializer=None)
 
+# 
 def deconv2d(input_, output_dim, ks=4, s=2, stddev=0.02, name="deconv2d"):
     with tf.variable_scope(name):
         return slim.conv2d_transpose(input_, output_dim, ks, s, padding='SAME', activation_fn=None,
